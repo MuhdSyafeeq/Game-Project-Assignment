8ca6e9f090f8676d8c1037043e9f74c331fd8c7b
cutieangel34 <59465809+cutieangel34@users.noreply.github.com>
2020-02-23 03:39:45 +0800

    uploaded

--- Project/index.js
+++ Project/index.js
@@ -24,10 +24,16 @@ var cursors;
 var fire;
 var platform;
 var spikes;
+var bat;
 
 //Health
 var health = 3;
 var healthText;
+
+//Coins
+var coins;
+var coinScore = 0;
+var coinText;
 
 //Status
 var gameOver = false;
@@ -40,17 +46,21 @@ var jumping;
 
 function preload() {
 	// Runs once, loads up assets like images and audio
-	//Clear fking cache
-	this.textures.remove('level1');
+	//Clear cache
+	//this.textures.remove('monsters');
 	
 	this.load.image("background","assets/images/4.png");
 	this.load.image("background2","assets/images/3.png");
+	this.load.image("star","assets/images/coin.png");
 	this.load.tilemapTiledJSON("level1","assets/tileMap/LevelA2.json");
 	this.load.image("tileSet","assets/tileSet/TilesSet16x16(E).png");
 	this.load.multiatlas('player', 'assets/mainCharacter.json', 'assets');
 	this.load.multiatlas('object', 'assets/object.json', 'assets'); 
+	this.load.multiatlas('monsters', 'assets/monsters.json', 'assets');
 	this.load.image("platform","assets/images/platform.png");
 	this.load.image("upSpikes","assets/images/UpSpikes.png");
+	this.load.setPath('assets/');
+	
 }
 
 function create() {
@@ -58,20 +68,23 @@ function create() {
 	this.add.image (0,0,"background").setOrigin(0,0);
 	this.add.image (0,0,"background2").setOrigin(0,0);
 	
-	map = this.make.tilemap({ key: "level1" });
+	const map = this.make.tilemap({ key: "level1" });
 	
 	// Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
 	// Phaser's cache (i.e. the name you used in preload)
-	tileset = map.addTilesetImage("TilesSet16x16(E)", "tileSet");
+	const tileset = map.addTilesetImage("TilesSet16x16(E)", "tileSet");
 
 	// Parameters: layer name (or index) from Tiled, tileset, x, y
-	behindLayer = map.createStaticLayer("World Background", tileset, 0, 0);
-	worldLayer = map.createStaticLayer("Player Path", tileset, 0, 0);	
+	const behindLayer = map.createStaticLayer("World Background", tileset, 0, 0);
+	const worldLayer = map.createStaticLayer("Player Path", tileset, 0, 0);	
 	spawnPoint = map.findObject("SpawnPoint", obj => obj.name === "Spawn Point");
+	var coinLayer = map.getObjectLayer('Collectibles')['objects'];
 	
 	//Player creation + animation
-	player = this.physics.add.sprite(spawnPoint.x, spawnPoint.y-10, 'player', 'Walking/0_Golem_Walking_000.png');
-	player.setSize(40,60, true);
+	player = this.physics.add.sprite(spawnPoint.x, spawnPoint.y-30, 'player', 'Walking/0_Golem_Walking_000.png');
+	player.setSize(40,70, true);
+	player.setBounce(0.2);
+	player.setDrag(50, 50);
 	
 	var walkFrames = this.anims.generateFrameNames('player', {
                          start: 0, end: 23, zeroPad: 3,
@@ -120,6 +133,42 @@ function create() {
 	fire.body.offset.y = 115;
 	fire.body.offset.x = 110;
 	
+	console.log(this.textures.get('monsters').frames);
+	
+	//Bat
+	
+	bat = this.physics.add.sprite(800, 420, 'monsters', 'bat/Fly/__Bat02_Fly_000.png').setVelocityX(50);
+	bat.body.setAllowGravity(false);
+	bat.setSize(115,60, true);
+	
+	var batWalkFrames = this.anims.generateFrameNames('monsters', {
+                         start: 0, end: 7, zeroPad: 3,
+                         prefix: 'bat/Fly/__Bat02_Fly_', suffix: '.png'
+                     });
+					 
+	this.anims.create({ key: 'batRight', frames: batWalkFrames, frameRate: 10, repeat: -1 });
+    bat.anims.play('batRight');
+	
+	var batWalkLeftFrames = this.anims.generateFrameNames('monsters', {
+                         start:0, end: 23, zeroPad: 3,
+                         prefix: 'bat/FlyLeft/__Bat02_Fly_', suffix: '.png'
+                     });
+					 
+	this.anims.create({ key: 'batLeft', frames: batWalkLeftFrames, frameRate: 10, repeat: -1 });
+    bat.anims.play('batLeft');
+	
+	//Coins
+	coins = this.physics.add.staticGroup()
+    //this is how we actually render our coin object with coin asset we loaded into our game in the preload function
+    
+	coinLayer.forEach(object => {
+    let obj = coins.create(object.x, object.y, "star"); 
+	   obj.setScale(object.width/16, object.height/16); 
+	   //obj.setOrigin(0); // Not needed here
+	   obj.body.width = object.width; 
+	   obj.body.height = object.height; 
+	});
+	
 	//Platform (move up and down)
 	platform = this.physics.add.image(450, 600, 'platform').setImmovable(true);
 	platform.body.setAllowGravity(false);
@@ -132,6 +181,7 @@ function create() {
 		  { x:    0, y:  150, duration: 2500, ease: 'Stepped' },
 		]
 	});
+	
 	
 	//Spikes
 	spikes = this.physics.add.image(342, 828, 'upSpikes').setImmovable(true).setScale(1.5);
@@ -146,8 +196,6 @@ function create() {
 	//this.physics.arcade.enable(player);
 	worldLayer.setCollisionByExclusion(-1, true);
 	player.setCollideWorldBounds(true);
-	player.setBounce(0.2);
-	player.setDrag(50, 50)
 	
 	// To simulate gravity on a sprite 
 	player.body.setGravityY(160);
@@ -155,18 +203,30 @@ function create() {
 	this.physics.add.collider(player, platform);
 	this.physics.add.collider(player, worldLayer);
 	this.physics.add.collider(fire, worldLayer);
+	//this.physics.add.collider(bat, worldLayer);
+	
+	//Overlap w monster
+	this.physics.add.overlap(player, bat, loseHealth, null, this);
 	//Collide with spikes
 	this.physics.add.collider(player, spikes, loseHealth, null, this);
 	//Overlap with fire
 	this.physics.add.overlap(player, fire, loseHealth, null, this);
+	//Overlap with coins
+	this.physics.add.overlap(player, coins, collectCoin, null, this);
 	
 	//Simple healthbar (text based)
-	healthText = this.add.text(1100, 280, `Health: ${health}`, {
+	healthText = this.add.text(1100, 290, `Health: ${health}`, {
       fontSize: '20px',
       fill: '#ffffff'
     });
     healthText.setScrollFactor(0);
 	
+	//Coin text
+	coinText = this.add.text(1100, 260, `Coins: ${coinScore}`, {
+      fontSize: '20px',
+      fill: '#ffffff'
+    });
+    coinText.setScrollFactor(0);
 	//Camera
 	this.cameras.main.startFollow(player).setFollowOffset(-250, 50).setZoom(1.8); //Camera follows player
 	
@@ -211,7 +271,7 @@ function update(time, delta) {
     }
 	
 	//onFloor()
-	if ( player.body.onFloor() ){
+	if ( player.body.onFloor() || player.body.touching.down ){
 		jumpLimit = 2;
 		jumping = false;
 	}
@@ -226,6 +286,19 @@ function update(time, delta) {
 	if ( jumping && cursors.up.isUp ) {
 		jumpLimit--;
 		jumping = false;
+	}
+	
+	//Patrolling bat
+	if ( bat.x > 900 ){
+		bat.setVelocityX(-50);
+	} else if ( bat.x < 750 ) {
+		bat.setVelocityX(50);
+	}
+		
+	if ( bat.body.velocity.x > 0){
+		bat.anims.play('batRight');	
+	} else {
+		bat.anims.play('batLeft');
 	}
 }
 
@@ -250,4 +323,11 @@ function updateHealth (){
 function checkStatus(){
 	player.clearTint();
 	losingHealth = false;
+}
+
+function collectCoin(player, coin) {
+    coin.destroy(coin.x, coin.y); // remove the tile/coin
+    coinScore ++; // increment the score
+    coinText.setText(`Coins: ${coinScore}`); // set the text to show the current score
+    return false;
 }
